f5e9195a4e7793990c5e8319db53567d3ffc9871
H-chanhee <83047523+H-chanhee@users.noreply.github.com>
2023-04-30 05:25:20 +0900

    logback-logging

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -4,9 +4,17 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.WeatherApplication;
+import zerobase.weather.domain.DateWeather;
 import zerobase.weather.domain.Diary;
+import zerobase.weather.repository.DateWeatherRepository;
 import zerobase.weather.repository.DiaryRepository;
 
 import java.io.BufferedReader;
@@ -19,6 +27,7 @@ import java.util.List;
 import java.util.Map;
 
 @Service
+@Transactional(readOnly = true)
 public class DiaryService {
 
 	/**
@@ -33,26 +42,71 @@ public class DiaryService {
 
 	private final DiaryRepository diaryRepository;
 
-	public DiaryService(DiaryRepository diaryRepository) {
+	private final DateWeatherRepository dateWeatherRepository;
+
+	private static final Logger logger =
+			LoggerFactory.getLogger(WeatherApplication.class);
+
+	public DiaryService(DiaryRepository diaryRepository,
+						DateWeatherRepository dateWeatherRepository) {
 		this.diaryRepository = diaryRepository;
+		this.dateWeatherRepository = dateWeatherRepository;
 	}
 
-	public void createDiary(LocalDate date, String text) {
+	@Transactional
+	@Scheduled(cron = "0 0 1 * * *")
+	public void saveWeatherDate() {
+		logger.info("날씨 데이터 가져오기 성공");
+		dateWeatherRepository.save(getWeatherFromApi());
+	}
+
+	private DateWeather getWeatherFromApi() {
 		String weatherData = getWeatherString();
 
 		Map<String, Object> parsedWeather = parseWeather(weatherData);
 
+		DateWeather dateWeather = new DateWeather();
+		dateWeather.setDate(LocalDate.now());
+		dateWeather.setWeather(parsedWeather.get("main").toString());
+		dateWeather.setIcon(parsedWeather.get("icon").toString());
+		dateWeather.setTemperature((Double) parsedWeather.get("temp"));
+
+		return dateWeather;
+	}
+
+	@Transactional(readOnly = false, isolation = Isolation.SERIALIZABLE)
+	public void createDiary(LocalDate date, String text) {
+//		String weatherData = getWeatherString();
+//		Map<String, Object> parsedWeather = parseWeather(weatherData);
+
+		logger.info("Started to create diary");
+		// 날씨 데이터 가져오기(API 에서 가져오기 OR DB 에서 가져오기) (캐싱 + )
+		DateWeather dateWeather = getDateWeather(date);
+
 		Diary nowDiary = new Diary();
-		nowDiary.setWeather(parsedWeather.get("main").toString());
-		nowDiary.setIcon(parsedWeather.get("icon").toString());
-		nowDiary.setTemperature((Double) parsedWeather.get("temp"));
+		nowDiary.setDateWeather(dateWeather);
 		nowDiary.setText(text);
 		nowDiary.setDate(date);
 
 		diaryRepository.save(nowDiary);
+		logger.info("End to create diary");
 	}
 
+	private DateWeather getDateWeather(LocalDate date) {
+		List<DateWeather> dateWeatherListFromDB =
+				dateWeatherRepository.findAllByDate(date);
+		if (dateWeatherListFromDB.size() == 0) {
+			// 새로 API 에서 날씨 정보를 가져와야 한다.
+			// 정책상 현재 날씨를 가져오도록 하거나 날씨 없이 일기를 써야한다.
+			return getWeatherFromApi();
+		} else {
+			return dateWeatherListFromDB.get(0);
+		}
+	}
+
+	@Transactional(readOnly = true)
 	public List<Diary> readDiary(LocalDate date) {
+		logger.debug("Read diary");
 		return diaryRepository.findAllByDate(date);
 	}
 
@@ -65,9 +119,11 @@ public class DiaryService {
 		nowDiary.setText(text);
 		diaryRepository.save(nowDiary);
 	}
+
 	public void deleteDiary(LocalDate date) {
 		diaryRepository.deleteAllByDate(date);
 	}
+
 	private String getWeatherString() {
 		String apiUrl =
 				"https://api.openweathermap.org/data/2.5/weather?q=incheon&appid="
